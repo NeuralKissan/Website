314c1b0bf3d8c71377ff40f8aa064d0159e57fc1
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-20 16:49:38 +0530

    Added WebP

--- resize.py
+++ resize.py
@@ -1,4 +1,4 @@
-import PIL
+from PIL import Image as PilImage
 import cv2
 import os
 
@@ -11,5 +11,6 @@ Images=os.listdir()
 
 for Image in Images:
     print(Image)
-    I=cv2.resize(cv2.imread(Image,cv2.IMREAD_UNCHANGED),(1200,1080))
+    I=cv2.resize(cv2.imread(Image,cv2.IMREAD_UNCHANGED),(600*(2),520*(2)))
     cv2.imwrite(f'Resized_{Image}',I)
+   
